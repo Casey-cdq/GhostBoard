5dbbbcbd009e3d4a5223fba2dc0408d3d533e075
Luckyhu <huji0624@gmail.com>
2019-06-27 15:00:41 +0800

    sug to client.
    fyi.

--- common.js
+++ common.js
@@ -85,7 +85,7 @@ function request_get(url,params,suc,fail){
         type: "GET",
         url: url,
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
-        dataType: "json",
+        dataType: "text",
         success: suc,
         error: fail
     });
--- render/online.js
+++ render/online.js
@@ -1,8 +1,9 @@
 var cm = require("../common")
 var remote = require('electron').remote
+const log = require('electron-log')
 
-String.prototype.strip = function() {
-  return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+function string_strip(str){
+	return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '')
 }
 
 // function ol_sug(key,mkt,func){
@@ -39,4 +40,67 @@ function __ol_get_keys(uuid,keys,suc,fail){
 	req_data.v = remote.app.getVersion()
 }
 
+function parse_sina_sug(text,m){
+	let sugs = []
+	let tk = text.split("=")[1]
+	tk = string_strip(tk.split('";')[0])
+	if (tk=="\""){
+		return sugs
+	}
+	let lines = tk.split(";")
+	for(i in lines){
+		let l = lines[i]
+		tk = l.split(",")
+		console.log(tk)
+
+		mkt = tk[1]
+		code = string_strip(tk[2])
+		mcode = string_strip(tk[3])
+		name = string_strip(tk[4])
+
+		if(m=="a" && mkt =="11"){
+			let sug = {}
+			sug['code'] = code
+            sug['name'] = name
+            sug['key'] = mcode+"@a"
+            sugs.push(sug)
+		}else if(m=="hk" && mkt =="31"){
+            let sug = {}
+			sug['code'] = code
+            sug['name'] = name
+            sug['key'] = mcode+"@hk"
+            sugs.push(sug)
+        }else{
+        	log.warn("sug warning:"+l)
+        }
+	}
+
+	return sugs
+}
+
+function __ol_sub(key,params,suc,fail){
+	let tk = key.split("@")
+	let k = tk[0]
+	let m = tk[1]
+
+	let now = new Date().getTime()
+	url = "https://suggest3.sinajs.cn/suggest/type=&key="+k+"&name=suggestdata_"+now
+
+	let req = cm.get(url,{},
+			 function (message) {
+		        console.log("OK:"+JSON.stringify(message))
+		        let ret = parse_sina_sug(message,m)
+		        console.log(ret)
+		        suc({value:ret})
+		    },
+		    function (message,einfo) {
+		        console.log("NOTOK:"+JSON.stringify(message))
+		        fail(message,einfo)
+		    }
+	)
+
+	return req
+}
+
 exports.get_keys = __ol_get_keys;
+exports.sug = __ol_sub;
