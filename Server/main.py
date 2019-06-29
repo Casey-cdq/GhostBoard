ccb8f511f8f8dc600ae826a6467825a8cc01062f
ji <236415625@qq.com>
2019-06-29 11:08:32 +0800

    f

--- Server/main.py
+++ Server/main.py
@@ -275,7 +275,11 @@ class index:
         parse_sina_text(datas,text)
         ret['datas'] = datas
         # ret['warning'] = "免费版目前只支持一只股票"
-        # ret['warning'] = 'new version <a onclick="cm.open_url(\'http://www.baidu.com\');" href="#">DD</a>.忽略消息请<a onclick="$(\'#warnalert\').remove();" href="#">关闭</a>'
+        newversion = '''
+        new version <a onclick="cm.open_url(\'http://www.baidu.com\');" href="#">DD</a>.
+        忽略消息请<a onclick="$(\'#warnalert\').remove();" href="#">关闭</a>
+        '''
+        ret['warning'] = newversion
         # print(ret)
         return json.dumps(ret)
 
